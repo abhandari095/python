2eaac37658b06a14fdd06a2061563d863adfd19a
abhandari095 <bhandariabhishek676@gmail.com>
2021-09-17 12:57:47 -0400

    changes

---
+++ inputfile.py
@@ -0,0 +1,121 @@
+#! /usr/bin/python3
+
+#import sys
+
+#print ("The no of arguments passed :" , len(sys.argv))
+#print ((sys.argv))
+#def inputvalue() :
+ #   print()
+  #  return;
+#inputvalue()
+
+""""
+input_name = input()
+
+print(input_name.strip())
+
+print(input_name.isdigit())
+
+print(input_name.strip().isdigit())
+
+
+
+if input_name.isdigit():
+    
+    print("Value is number")
+else: 
+    print("The value is string")
+
+print("The total length of the input is :", len(input_name))
+
+
+name = "Twinkle, twinkle, little star, \n\t How I wonder what you are! \n\t Up above the world so high, \n\t Like a diamond in the sky. \n\t Twinkle, twinkle, little star, How I wonder what you are"
+
+print(name)
+
+
+import datetime
+now = datetime.datetime.now()
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
+"""
+
+"""
+# Write a function without parameter which will print the values of input
+
+#input_name = input()
+import getpass
+
+def abcparam ():
+    input_value = getpass.getpass('Enter your value:   ')
+    print("the value of return value is: " ,input_value)
+    return;
+
+abcparam()
+    
+"""
+
+#write function with parameter for multiple and division
+
+"""
+print("Enter first number:")
+a = input()
+print("Enter second number:")
+b= input()
+
+def multiply(a,b):
+    #print("Enter first number:")
+    a = int(a)
+    #print("Enter second number:")
+    b = int(b)
+    multi = a * b
+    print("Answer of multipication is :" , multi)
+
+def divide(a,b):
+    a = int(a)
+    #print("Enter second number:")
+    b = int(b)
+    #a = int(input())
+    #print("Enter second number:")
+    #b = int(input())
+    divide = a / b
+    print("Answer of division is :" , divide)
+
+    
+multiply (a,b)
+divide (a,b)
+
+
+"""
+# write function which makes a character uppercase and lowercase
+
+"""
+print("enter a char")
+a = input()
+def uppercase() :
+      
+    print("Converting to uppercase")
+    print (a.upper())
+    
+def lowercase() :
+     print("Converting to lowercase" )    
+     print(a.lower())
+    
+uppercase()    
+lowercase()
+  
+"""""
+# Compare whether it is a charecter or string
+
+print ("Please enter a value:")
+
+a = input()
+
+if a.isstring :
+    print ("true")
+    
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,15 @@
+l = ['a','b','c','d','e','f','g','h','i']
+print(l[2:7])
+print(l[1:7])
+
+
+
+
+{“destinations” “ [
+	{“environment”:”DEV”,”server”: “DEV52653.cloud.org"},
+	{“environment”:”UATInternal”,”server”: “UAT12135.cloud.org”},
+	{“environment”:”UATExternal”,”server”: “UAT1112212.cloud.org"},
+	{“environment”:”ProdInternal”,”server”: “PRD526353.cloud.org"},
+	{“environment”:”ProdExternal”,”server”: “PRD5265d3.cloud.org"},
+]}
+
